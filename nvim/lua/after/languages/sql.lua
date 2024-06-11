f3fe9f5ce92ddec1634cb3a72f3776ebff3d9c78
ivn <ivn@localhost.localdomain>
2024-06-11 18:23:14 -0400

    added sql ls support

---
+++ nvim/lua/after/languages/sql.lua
@@ -0,0 +1,13 @@
+-- Python SETUP LSP
+local lsp_helpers = require("after.languages.lsp")
+local on_attach = lsp_helpers.on_attach
+--Enable (broadcasting) snippet capability for completion
+local capabilities = vim.lsp.protocol.make_client_capabilities()
+capabilities.textDocument.completion.completionItem.snippetSupport = true
+
+require("lspconfig").sqlls.setup({
+	capabilities = capabilities,
+	on_attach = on_attach,
+	cmd = { "sql-language-server", "up", "--method", "stdio" },
+	filetypes = { "sql", "mysql" },
+})
